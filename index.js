3d6156728431a67b05379f2fa37737a73f85fa91
zhujunxi <531578783@qq.com>
2019-07-25 20:45:49 +0800

    [feat] 实现简单的路由拦截，识别请求方法

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const Koa = require("koa");
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = "hello zhihu API";
+app.use(async ctx => {
+  if (ctx.url === "/") {
+    ctx.body = "这是主页";
+  } else if (ctx.url === "/users") {
+    if (ctx.method == "GET") {
+      ctx.body = "这是用户列表页";
+    } else if (ctx.method == "POST") {
+      ctx.body = "创建用户";
+    } else {
+      ctx.status = 405;
+    }
+  } else if (ctx.url.match(/\/users\/\w+/)) {
+    const userId = ctx.url.match(/\/users\/\w+/);
+    ctx.body = `这是用户 ${userId} ${ctx.url}`;
+  } else {
+    ctx.status = 404;
+  }
 });
 
 app.listen(3000);
