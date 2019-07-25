022f882064ee062b3833117a5f9869e36b3d8cec
zhujunxi <531578783@qq.com>
2019-07-25 20:54:30 +0800

    [feat] 新增koa-router

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-  if (ctx.url === "/") {
-    ctx.body = "这是主页";
-  } else if (ctx.url === "/users") {
-    if (ctx.method == "GET") {
-      ctx.body = "这是用户列表页";
-    } else if (ctx.method == "POST") {
-      ctx.body = "创建用户";
-    } else {
-      ctx.status = 405;
-    }
-  } else if (ctx.url.match(/\/users\/\w+/)) {
-    const userId = ctx.url.match(/\/users\/\w+/);
-    ctx.body = `这是用户 ${userId} ${ctx.url}`;
-  } else {
-    ctx.status = 404;
-  }
+router.get("/", ctx => {
+  ctx.body = "这是主页";
 });
 
+router.get("/users", ctx => {
+  ctx.body = "这是用户列表";
+});
+
+app.use(router.routes());
 app.listen(3000);
