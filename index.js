931856bd09c4dd1a72e250551dde575f36198340
zhujunxi <531578783@qq.com>
2019-07-25 20:58:01 +0800

    [feat] 使用koa-router的prefix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const Router = require("koa-router");
 const app = new Koa();
 const router = new Router();
+const userRouter = new Router({ prefix: "/users" });
 
 router.get("/", ctx => {
   ctx.body = "这是主页";
@@ -11,5 +12,13 @@ router.get("/users", ctx => {
   ctx.body = "这是用户列表";
 });
 
+userRouter.post("/users", ctx => {
+  ctx.body = "这是创建用户";
+});
+
+userRouter.get("/users/:id", ctx => {
+  ctx.body = ctx.params.id;
+});
 app.use(router.routes());
+app.use(userRouter.routes());
 app.listen(3000);
