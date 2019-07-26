4e31f35ddf30a918542ea9b83ec8602fd84d6bb1
zhujunxi <531578783@qq.com>
2019-07-26 10:06:26 +0800

    [feat] 添加koa-router的增删改查方法

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ const app = new Koa();
 const router = new Router();
 const userRouter = new Router({ prefix: "/users" });
 
-const auth = async (ctx, next) => {
-  if (ctx.url !== "/users") {
-    ctx.throw(401);
-  }
-  await next();
-};
-
-router.get("/", auth, ctx => {
+router.get("/", ctx => {
   ctx.body = "这是主页";
 });
 
-userRouter.get("/", auth, ctx => {
-  ctx.body = "这是用户列表";
+userRouter.get("/", ctx => {
+  ctx.body = [{ name: "lilei" }, { name: "hanmeimei" }];
 });
 
-userRouter.post("/", auth, ctx => {
-  ctx.body = "这是创建用户";
+userRouter.post("/", ctx => {
+  ctx.body = { name: "lilei" };
 });
 
 userRouter.get("/:id", ctx => {
-  ctx.body = ctx.params.id;
+  ctx.body = { name: "lilei" };
 });
+
+userRouter.put("/:id", ctx => {
+  ctx.body = { name: "lilei2" };
+});
+
+userRouter.delete("/:id", ctx => {
+  ctx.body = 204;
+});
+
 app.use(router.routes());
 app.use(userRouter.routes());
 
